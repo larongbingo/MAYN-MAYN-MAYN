dede89f7a9276c70d937074a36235eb7ac2fdf2c
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 19:18:34 +0800

    Fetch data from MAYN Hasura

--- functions/userCreate.js
+++ functions/userCreate.js
@@ -3,6 +3,8 @@ const { hash } = require('bcryptjs');
 exports.handler = async function(event, context, callback) {
   console.log(event);
   
+  const url_root = 'https://mayn-mayn-mayn-chat.herokuapp.com/v1alpha1/graphql';
+
   if(event.httpMethod !== 'POST') {
     return { 
       statusCode: 400,
@@ -12,8 +14,30 @@ exports.handler = async function(event, context, callback) {
     }
   }
   
+  const query = `query {
+    users(where: {username: {_eq: "user"}}) {
+      id
+      username
+      password
+    }
+  }`;
+
   // Check if the username is unique
+  let response = await fetch(url_root, {
+    method: 'GET',
+    headers: {
+      'X-Hasura-Access-Key': 'ThatOneNoob',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({query: query})
+  });
+
+  let result = response.json();
+
+  console.log(result);
+
   // Hash the password
+
 
   return {
     statusCode: 200,
