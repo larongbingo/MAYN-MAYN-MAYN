3825fa6cdb6d93e2f1a27f60a30b3d2595124ac4
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 15:34:18 +0800

    Testing for async serverless

--- functions/userCreate.js
+++ functions/userCreate.js
@@ -1,7 +1,19 @@
 const { hash } = require('bcryptjs');
 
 exports.handler = async function(event, context, callback) {
-  console.log(event, context, callback);
-  callback(null, event);
-  return event;
+  console.log(event);
+  
+  if(event.httpMethod !== 'POST') {
+    return { 
+      statusCode: 400,
+      payload: {
+        message: 'Invalid HTTP Method'
+      }  
+    }
+  }
+  
+  return {
+    statusCode: 200,
+    payload: event
+  };
 }
