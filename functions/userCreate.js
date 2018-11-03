aa1d03622dbf15562b03eba94b9de34140bcfd6d
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 14:41:58 +0800

    Added a return statement

--- functions/userCreate.js
+++ functions/userCreate.js
@@ -3,4 +3,5 @@ const { hash } = require('bcryptjs');
 exports.handler = async function(event, context, callback) {
   console.log(event, context, callback);
   callback(null, event);
+  return event;
 }
