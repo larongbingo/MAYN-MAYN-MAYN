6a8d8422213b15f3c276aa47360dcc91e922f45a
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 12:15:37 +0800

    Cleanup on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1 style={{textAlign: "center"}}>MAYN! MAYN! MAYN!</h1>
+    <h3 style={{textAlign: "center"}}>An auction web app where you have to say 'MINE!' to place a bid</h3>
   </Layout>
 )
 
