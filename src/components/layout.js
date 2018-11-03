25f3abcae4be8fc64c3065427c493cb3a5f188cb
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 11:18:09 +0800

    Cleanup on layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { Grid } from 'react-bootstrap'
 
 import Header from './header'
-import './layout.css'
+import HelmetConfig from './helmet'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,26 +19,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
+        <HelmetConfig title={data.site.siteMetadata.title} />
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <Grid>
           {children}
-        </div>
+        </Grid>
       </>
     )}
   />
