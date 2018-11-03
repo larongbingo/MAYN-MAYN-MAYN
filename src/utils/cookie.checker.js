e847b8340c45b688c1ddf8de2bc6d2c529fc9353
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-03 14:11:53 +0800

    Added a function to check if theres a session or not

---
+++ src/utils/cookie.checker.js
@@ -0,0 +1,9 @@
+
+/**
+ * Checks whether theres a cookie that signifies that theres
+ * am active session
+ * @returns {boolean} Returns true if theres an active session, false otherwise
+ */
+export function cookieChecker() {
+  return !localStorage.getItem('session');
+}
