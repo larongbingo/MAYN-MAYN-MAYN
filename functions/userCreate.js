ab3fb9f5943452e13eee62455831b07476cdc892
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-04 12:32:14 +0800

    User creation finally works T_T

--- functions/userCreate.js
+++ functions/userCreate.js
@@ -1,17 +1,35 @@
-const client = require('./utils/graphql.client')
+const fetch = require('node-fetch');
 
-exports.handler = async function(event, context, callback) {
+exports.handler = async function(event, context) {
   console.log(event);
-  
-  const query = `query {
-    users(where: {username: {_eq: "user"}}) {
-      id
-      username
-      password
+
+  console.log(JSON.parse(event.body));
+
+  const credentials = JSON.parse(event.body);
+
+  const url = "https://mayn-mayn-mayn-chat.herokuapp.com/v1alpha1/graphql";
+
+  const headers = {
+    "X-Hasura-Access-Key": "ThatOneNoob",
+    "Content-Type": "application/json"
+  }
+
+  const query = `mutation {
+    insert_users(objects: [{username: "${credentials.username}", password:"${credentials.password}"}]) {
+      affected_rows
+      returning {
+        id
+      }
     }
   }`;
 
-  let result = await client.query({operationName: 'Query', query: query, variables: null});
+  let response = await fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({query: query})
+  })
+
+  let result = await response.json()
 
   return {
     statusCode: 200,
