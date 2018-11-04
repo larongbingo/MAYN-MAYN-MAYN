0046a36073ef3b2253f1f43eef996eefdfacdda8
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-04 23:09:13 +0800

    Added a constants file

---
+++ functions/utils/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Pilon_api_url_customers: "https://api.pilon.io/v1/customers",
+  Hasura_api_url: "https://mayn-mayn-mayn-chat.herokuapp.com/v1alpha1/graphql",
+  environment_id: "28ac04ce-e01a-11e8-8947-c180050c48e3"
+}
