bd73bd3d64248892a253fa319014f947b7c3e687
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-04 16:50:15 +0800

    Added logout and shopping cart on a valid session

--- src/components/account.header.js
+++ src/components/account.header.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Nav, NavItem } from 'react-bootstrap'
 
-export const AccountHeader = () => (
-  <Nav pullRight>
-    <NavItem eventKey={1} href="/login">Log In</NavItem>
-    <NavItem eventKey={2} href="/signup">Sign Up</NavItem>
-  </Nav>
-);
+import CookieCheck from '../utils/cookie.checker'
+
+export const AccountHeader = () => {
+  if(CookieCheck()) {
+    return (
+      <Nav pullRight>
+        <NavItem eventKey={1} href="/shoppingbag">Cart</NavItem>
+        <NavItem eventKey={2} href="/logout">Log Out</NavItem>
+      </Nav>
+    )
+  }
+  
+  return (
+    <Nav pullRight>
+      <NavItem eventKey={1} href="/login">Log In</NavItem>
+      <NavItem eventKey={2} href="/signup">Sign Up</NavItem>
+    </Nav>
+  )
+};
 
 export default AccountHeader;
