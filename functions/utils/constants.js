b4c72e2f3709380b99185ea0b7258998d3703f53
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-05 01:40:01 +0800

    Added auth function

--- functions/utils/constants.js
+++ functions/utils/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
   Pilon_api_url_customers: "https://api.pilon.io/v1/customers",
+  Pilon_api_url_auth: "https://api.pilon.io/v1/token",
   Hasura_api_url: "https://mayn-mayn-mayn-chat.herokuapp.com/v1alpha1/graphql",
   environment_id: "28ac04ce-e01a-11e8-8947-c180050c48e3"
 }
