e0be3c4c78ccd17144cbae6a74953d9fdc7d900b
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-05 17:03:32 +0800

    Reset of code

--- src/components/account.header.js
+++ src/components/account.header.js
@@ -1,18 +1,7 @@
 import React from 'react'
 import { Nav, NavItem } from 'react-bootstrap'
 
-import CookieCheck from '../utils/cookie.checker'
-
 export const AccountHeader = () => {
-  if(CookieCheck()) {
-    return (
-      <Nav pullRight>
-        <NavItem eventKey={1} href="/shoppingbag">Cart</NavItem>
-        <NavItem eventKey={2} href="/logout">Log Out</NavItem>
-      </Nav>
-    )
-  }
-  
   return (
     <Nav pullRight>
       <NavItem eventKey={1} href="/login">Log In</NavItem>
