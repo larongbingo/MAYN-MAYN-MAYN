c14a6c76f2a340157d19b5c9cef5d5763693a4ba
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2018-11-05 09:11:52 +0800

    Added logout page

---
+++ src/pages/logout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import cookies from '../utils/cookies'
+import Layout from '../components/layout'
+
+export class LogOut extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    cookies.remove('session');
+
+    window.location = '/'
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h1>Logging Out</h1>
+      </Layout>
+    )
+  }
+}
